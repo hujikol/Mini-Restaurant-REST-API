c409e9877aea4750ded49eeab697a5a2ee99bc25
hujikol <nikolasnanda@gmail.com>
2023-04-07 13:19:04 +0700

    add comments

--- app/crud/kategori.py
+++ app/crud/kategori.py
@@ -5,58 +5,75 @@ from models import models
 
 
 def create_kategori(db: Session, kategori: schemas.KategoriCreate):
+    # Check if the category already exists in the database
     db_kategori = db.query(models.Kategori).filter(
         models.Kategori.nama_kat == kategori.nama_kat).first()
 
     if db_kategori:
+        # If the category already exists, return an error message with a 409 status code
         return {"message": f"Kategori with name {kategori.nama_kat} already exists"}, 409
 
+    # Create a new category object and add it to the database
     db_kategori = models.Kategori(nama_kat=kategori.nama_kat)
     db.add(db_kategori)
     db.commit()
     db.refresh(db_kategori)
 
+    # Return a success message with the newly created category and a 201 status code
     return {"message": "New kategori created", "kategori": db_kategori}, 201
 
 
 def get_kategori(db: Session, kategori_id: int):
+    # Get a category from the database based on its ID
     db_kategori = db.query(models.Kategori).filter(
         models.Kategori.id == kategori_id).first()
 
     if not db_kategori:
+        # If the category is not found, return an error message with a 404 status code
         return {"message": f"Kategori with id {kategori_id} is not found"}, 404
 
+    # Return a success message with the found category and a 200 status code
     return {"message": "Kategori found", "data": db_kategori}, 200
 
 
 def get_kategori_by_name(db: Session, nama_kat: str):
+    # Get a category from the database based on its name
     db_kategori = db.query(models.Kategori).filter(
         models.Kategori.nama_kat == nama_kat).first()
 
     if not db_kategori:
+        # If the category is not found, return an error message with a 404 status code
         return {"message": f"Kategori with name {nama_kat} is not found"}, 404
 
+    # Return a success message with the found category and a 200 status code
     return {"message": "Kategori found", "data": db_kategori}, 200
 
 
 def get_all_kategori(db: Session, offset: int = 0, page_size: int = 100):
+    # Get all categories from the database with pagination
     db_kategori = db.query(models.Kategori).offset(
         offset).limit(page_size).all()
 
+    # Get the total number of categories in the database
     total_data = db.query(models.Kategori).count()
 
+    # Return a success message with all categories and a 200 status code
     return {"message": "All kategori returned", "total_data": total_data, "data": db_kategori}, 200
 
 
 def update_kategori(db: Session, kategori_id: int, kategori: schemas.KategoriUpdate):
+    # Get a category from the database based on its ID
     db_kategori = db.query(models.Kategori).filter_by(id=kategori_id).first()
 
     if not db_kategori:
+        # If the category is not found, return an error message with a 404 status code
         return {"message": f"Kategori with id {kategori_id} is not found"}, 404
 
     if not any([kategori.nama_kat]):
+        # If no fields are provided to update, return an error message with a 400 status code
         return {"message": "At least one field is required to update kategori"}, 400
 
+    # Update the category object with the provided data
     update_data = {k: v for k, v in kategori.dict(exclude_unset=True).items()}
     for key, value in update_data.items():
         setattr(db_kategori, key, value)
@@ -64,16 +81,21 @@ def update_kategori(db: Session, kategori_id: int, kategori: schemas.KategoriUpd
     db.commit()
     db.refresh(db_kategori)
 
+    # Return a success message with the updated category and a 200 status code
     return {"message": f"Kategori with id {kategori_id} is updated", "data": db_kategori}, 200
 
 
 def delete_kategori(db: Session, kategori_id: int):
+    # Get a category from the database based on its ID
     db_kategori = get_kategori(db, kategori_id)["data"]
 
     if not db_kategori:
+        # If the category is not found, return an error message with a 404 status code
         return {"message": f"Kategori with id {kategori_id} is not found"}, 404
 
+    # Delete the category object from the database
     db.delete(db_kategori)
     db.commit()
 
-    return {"message": f"Kategori with id {kategori_id} is deleted"}, 200
+    # Return a success message with the deleted category and a 200 status code
+    return {"message": f"Kategori with id {kategori_id} is deleted"}, 204
--- app/crud/resep.py
+++ app/crud/resep.py
@@ -4,49 +4,66 @@ from models import models
 
 
 def create_resep(db: Session, resep: schemas.ResepCreate):
+    # Check if required fields are present in the request body
     if not resep.nama_resep or not resep.kategori_id:
         return {"message": "Nama resep and kategori ID are required"}, 400
 
+    # Check if a resep with the same name already exists
     db_resep = db.query(models.Resep).filter(
         models.Resep.nama_resep == resep.nama_resep).first()
+
     if db_resep:
+        # If the resep is not found in the database, return a 404 error message
         return {"message": f"Resep with name {resep.nama_resep} already exists"}, 409
 
+    # Create a new resep object and add it to the database
     db_resep = models.Resep(nama_resep=resep.nama_resep,
                             kategori_id=resep.kategori_id)
     db.add(db_resep)
     db.commit()
     db.refresh(db_resep)
 
+    # Return success response with the newly created resep object
     return {"message": "New resep created", "data": db_resep}, 201
 
 
 def get_resep(db: Session, resep_id: int):
+    # Query the database for the resep with the given ID
     db_resep = db.query(models.Resep).get(resep_id)
+
     if not db_resep:
+        # If the resep is not found in the database, return a 404 error message
         return {"message": f"Resep with ID {resep_id} is not found"}, 404
 
+    # Return success response with the resep object
     return {"message": "Resep found", "data": db_resep}, 200
 
 
 def get_resep_by_name(db: Session, nama_resep: str):
+    # Query the database for the resep with the given name
     db_resep = db.query(models.Resep).filter(
         models.Resep.nama_resep == nama_resep).first()
+
     if not db_resep:
+        # If the resep is not found in the database, return a 404 error message
         return {"message": f"Resep with name {nama_resep} is not found"}, 404
 
+    # Return success response with the resep object
     return {"message": f"Resep with name {nama_resep} is found", "data": db_resep}, 200
 
 
-def get_all_resep(db: Session, offset: int = 0, page_size: int = 100, nama_resep: str = None, kategori_id: int = None, sort_by: str = None, sort_order: str = "asc"):
+def get_all_resep(db: Session, offset: int = 0, page_size: int = 100, nama_resep: str = None, kategori_id: int = None, sort_by: str = "id", sort_order: str = "asc"):
+    # Start building the query with the Resep model
     query = db.query(models.Resep)
     total_data = query.count()
 
+    # Apply filters based on optional parameters
     if nama_resep:
         query = query.filter(models.Resep.nama_resep.ilike(f"%{nama_resep}%"))
     if kategori_id:
         query = query.filter(models.Resep.kategori_id == kategori_id)
 
+    # Apply sorting based on optional parameters
     if sort_by:
         column = getattr(models.Resep, sort_by, None)
         if column is not None:
@@ -54,38 +71,53 @@ def get_all_resep(db: Session, offset: int = 0, page_size: int = 100, nama_resep
                 column = column.desc()
             query = query.order_by(column)
 
+    # Execute the query with pagination
     db_resep = db_resep.offset(offset).limit(page_size).all()
 
+    # Check if any reseps were found
     if len(db_resep) == 0:
         return {"message": "No resep found"}, 404
 
-    return {"message": "All resep retrieved", "data": db_resep, "total_data": total_data}, 200
+    # Return success response with the list of reseps and total count
+    return {"message": "All resep retrieved", "total_data": total_data, "data": db_resep}, 200
 
 
 def update_resep(db: Session, resep_id: int, resep: schemas.ResepUpdate):
+    # Query the database for the resep with the given ID
     db_resep = db.query(models.Resep).get(resep_id)
+
     if not db_resep:
+        # If the resep is not found in the database, return a 404 error message
         return {"message": f"Resep with ID {resep_id} is not found"}, 404
 
+    # Check if any fields are being updated
     if not any([resep.nama_resep, resep.kategori_id]):
         return {"message": "At least one field is required to update resep"}, 400
 
+    # Update the resep object with the new data
     update_data = {k: v for k, v in resep.dict(exclude_unset=True).items()}
     for key, value in update_data.items():
         setattr(db_resep, key, value)
 
+    # Commit the changes to the database
     db.commit()
     db.refresh(db_resep)
 
+    # Return success response with the updated resep object
     return {"message": f"Resep with id {resep_id} is updated", "data": db_resep}, 200
 
 
 def delete_resep(db: Session, resep_id: int):
+    # Query the database for the resep with the given ID
     db_resep = db.query(models.Resep).filter_by(id=resep_id).first()
+
     if not db_resep:
+        # If the resep is not found in the database, return a 404 error message
         return {"message": f"Resep with ID {resep_id} is not found"}, 404
 
+    # Delete the resep object from the database
     db.delete(db_resep)
     db.commit()
 
-    return {"message": "Resep deleted successfully"}, 204
+    # Return success response with no content
+    return {"message": f"Resep with id {resep_id} is deleted successfully"}, 204
