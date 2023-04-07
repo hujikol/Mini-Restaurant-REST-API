2dbe181629fc7235b47f133443fa94232783af1f
hujikol <nikolasnanda@gmail.com>
2023-04-07 11:20:16 +0700

    clearer code,adjust update, stadarize return data

--- app/crud/kategori.py
+++ app/crud/kategori.py
@@ -5,46 +5,75 @@ from models import models
 
 
 def create_kategori(db: Session, kategori: schemas.KategoriCreate):
-    db_kategori = db.query(models.Kategori).filter(models.Kategori.nama_kat == kategori.nama_kat).first()
+    db_kategori = db.query(models.Kategori).filter(
+        models.Kategori.nama_kat == kategori.nama_kat).first()
+
     if db_kategori:
-        return {"error": {"message": f"Kategori with name {kategori.nama_kat} already exists"}}, 409
+        return {"message": f"Kategori with name {kategori.nama_kat} already exists"}, 409
+
     db_kategori = models.Kategori(nama_kat=kategori.nama_kat)
     db.add(db_kategori)
     db.commit()
     db.refresh(db_kategori)
-    return {"success": {"message": "New kategori created"}, "data": db_kategori}, 201
+
+    return {"message": "New kategori created", "kategori": db_kategori}, 201
+
 
 def get_kategori(db: Session, kategori_id: int):
-    db_kategori = db.query(models.Kategori).filter(models.Kategori.id == kategori_id).first()
+    db_kategori = db.query(models.Kategori).filter(
+        models.Kategori.id == kategori_id).first()
+
     if not db_kategori:
-        return {"error": {"message": f"Kategori with id {kategori_id} is not found"}}, 404
-    return {"success": {"message": "Kategori found"}, "data": db_kategori}, 200
+        return {"message": f"Kategori with id {kategori_id} is not found"}, 404
+
+    return {"message": "Kategori found", "data": db_kategori}, 200
+
 
 def get_kategori_by_name(db: Session, nama_kat: str):
-    db_kategori = db.query(models.Kategori).filter(models.Kategori.nama_kat == nama_kat).first()
-    if not db_kategori:
-        return {"error": {"message": f"Kategori with name {nama_kat} is not found"}}, 200
-    return {"success": {"message": "Kategori found"}, "data": db_kategori}, 200
+    db_kategori = db.query(models.Kategori).filter(
+        models.Kategori.nama_kat == nama_kat).first()
 
-def get_all_kategori(db: Session, skip: int = 0, limit: int = 100):
-    db_kategori = db.query(models.Kategori).offset(skip).limit(limit).all()
-    return {"success": {"message": "All kategori returned"}, "data": db_kategori}, 200
+    if not db_kategori:
+        return {"message": f"Kategori with name {nama_kat} is not found"}, 404
+
+    return {"message": "Kategori found", "data": db_kategori}, 200
+
+
+def get_all_kategori(db: Session, offset: int = 0, page_size: int = 100):
+    db_kategori = db.query(models.Kategori).offset(
+        offset).limit(page_size).all()
+
+    total_data = db.query(models.Kategori).count()
+
+    return {"message": "All kategori returned", "total_data": total_data, "data": db_kategori}, 200
+
 
 def update_kategori(db: Session, kategori_id: int, kategori: schemas.KategoriUpdate):
-    db_kategori = get_kategori(db, kategori_id)["data"]
+    db_kategori = db.query(models.Kategori).filter_by(id=kategori_id).first()
+
     if not db_kategori:
-        return {"error": {"message": f"Kategori with id {kategori_id} is not found"}}, 200
-    for var, value in kategori.__dict__.items():
-        if value is not None:
-            setattr(db_kategori, var, value)
+        return {"message": f"Kategori with id {kategori_id} is not found"}, 404
+
+    if not any([kategori.nama_kat]):
+        return {"message": "At least one field is required to update kategori"}, 400
+
+    update_data = {k: v for k, v in kategori.dict(exclude_unset=True).items()}
+    for key, value in update_data.items():
+        setattr(db_kategori, key, value)
+
     db.commit()
     db.refresh(db_kategori)
-    return {"success": {"message": f"Kategori with id {kategori_id} is updated"}, "data": db_kategori}, 200
+
+    return {"message": f"Kategori with id {kategori_id} is updated", "data": db_kategori}, 200
+
 
 def delete_kategori(db: Session, kategori_id: int):
     db_kategori = get_kategori(db, kategori_id)["data"]
+
     if not db_kategori:
-        return {"error": {"message": f"Kategori with id {kategori_id} is not found"}}, 200
+        return {"message": f"Kategori with id {kategori_id} is not found"}, 404
+
     db.delete(db_kategori)
     db.commit()
-    return {"success": {"message": f"Kategori with id {kategori_id} is deleted"}, "data": db_kategori}, 200
+
+    return {"message": f"Kategori with id {kategori_id} is deleted"}, 200
